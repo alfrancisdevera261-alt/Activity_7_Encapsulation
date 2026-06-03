c3279bd5533dcb5845949112868f76f2aa3c830b
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-06-03 22:04:44 +0800

    prints a formatted separator line with an optional title

---
+++ Excercise 2/car_test.py
@@ -0,0 +1,9 @@
+from car import Car
+
+def print_separator(title=""):
+    width = 55
+    if title:
+        padding = (width - len(title) - 2) // 2
+        print("=" * padding + f" {title} " + "=" * padding)
+    else:
+        print("=" * width)
