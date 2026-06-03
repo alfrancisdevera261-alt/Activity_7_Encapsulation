dd67f68cf205160deebd822537d4959920be638d
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-06-03 22:21:41 +0800

    Only call the setter if the caller actually passed a value.

--- Excercise 3/pet.py
+++ Excercise 3/pet.py
@@ -6,3 +6,10 @@ class Pet:
         self.__name        = ""
         self.__animal_type = ""
         self.__age         = 0
+        
+        if name:
+            self.set_name(name)
+        if animal_type:
+            self.set_animal_type(animal_type)
+        if age:
+            self.set_age(age)
