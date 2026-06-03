5ef1309208279c0b5de78c0bbff56380f0c9f0a5
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-06-03 20:53:54 +0800

    make a constructor runs automatically when we write Fan(), and If no speed was passed in, default to SLOW.

--- Exercise 1/fan.py
+++ Exercise 1/fan.py
@@ -5,3 +5,7 @@ class Fan:
     FAST   = 3   
     
     _SPEED_LABELS = {1: "SLOW", 2: "MEDIUM", 3: "FAST"}
+    
+    def __init__(self, speed=None, radius=5.0, color="blue", on=False):
+        if speed is None:
+            speed = Fan.SLOW
