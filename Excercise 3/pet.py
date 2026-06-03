bec7e4d541523d8700a227db86857e4dadfbdbb4
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-06-03 22:21:10 +0800

    Define the constructor, and all three parameters have default empty values so you can create

--- Excercise 3/pet.py
+++ Excercise 3/pet.py
@@ -2,3 +2,7 @@ VALID_TYPES = {"Dog", "Cat", "Bird", "Fish", "Rabbit",
                "Hamster", "Turtle", "Snake", "Lizard", "Other"}
 
 class Pet:
+    def __init__(self, name: str = "", animal_type: str = "", age: int = 0):
+        self.__name        = ""
+        self.__animal_type = ""
+        self.__age         = 0
