fb586bc5b3f20be55a1897f7e6d2f2a5c9e187b7
Alfrancis C. De Vera <alfrancidevera261@gmail.com>
2026-06-03 22:32:00 +0800

    prints a formatted separator line

--- Excercise 3/pet_test.py
+++ Excercise 3/pet_test.py
@@ -1,1 +1,8 @@
 from pet import Pet, VALID_TYPES
+def print_separator(title=""):
+    width = 55
+    if title:
+        padding = (width - len(title) - 2) // 2
+        print("=" * padding + f" {title} " + "=" * padding)
+    else:
+        print("=" * width)
